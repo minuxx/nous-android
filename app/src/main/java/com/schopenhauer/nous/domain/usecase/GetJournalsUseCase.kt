b41ea040a74d004df78223ab7d5ecc34687e4796
minuxx <minux8905@gmail.com>
2024-02-25 08:18:01 +0900

    :sparkles: feat: 업무 일지 최신순 정렬 로직 추가
    Resolves: #19

--- app/src/main/java/com/schopenhauer/nous/domain/usecase/GetJournalsUseCase.kt
+++ app/src/main/java/com/schopenhauer/nous/domain/usecase/GetJournalsUseCase.kt
@@ -1,8 +1,11 @@
 package com.schopenhauer.nous.domain.usecase
 
+import android.util.Log
 import com.schopenhauer.nous.domain.mapper.toJournal
 import com.schopenhauer.nous.domain.repository.JournalsRepository
+import com.schopenhauer.nous.util.ErrorType.PARSE
 import com.schopenhauer.nous.util.Result
+import com.schopenhauer.nous.util.parseDate
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -12,8 +15,21 @@ class GetJournalsUseCase @Inject constructor(
 ) {
 	suspend operator fun invoke() = withContext(Dispatchers.Default) {
 		when(val res = journalsRepository.getJournals()) {
-			is Result.Success -> Result.Success(res.data?.map { it.toJournal() } ?: emptyList())
+			is Result.Success -> {
+				try {
+					val journals = res.data?.map { it.toJournal() } ?: emptyList()
+					val sortedJournals = journals.sortedByDescending { parseDate(it.date) }
+					Result.Success(sortedJournals)
+				} catch (e: Exception) {
+					Log.e(TAG, "${PARSE.message}, Message: ${e.message}")
+					Result.Error(PARSE.code, PARSE.message)
+				}
+			}
 			is Result.Error -> res
 		}
+	}
+
+	companion object {
+		private const val TAG = "GetJournalsUseCase"
 	}
 }
--- app/src/main/java/com/schopenhauer/nous/util/DateUtill.kt
+++ app/src/main/java/com/schopenhauer/nous/util/DateUtill.kt
@@ -1,7 +1,9 @@
 package com.schopenhauer.nous.util
 
 import android.os.Build
+import android.util.Log
 import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.time.Instant
 import java.time.ZoneId
@@ -9,14 +11,26 @@ import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
 
+private const val DATE_PATTERN = "yyyy/MM/dd"
+private const val TAG = "DateUtil"
+
 fun millisToDate(millis: Long = MaterialDatePicker.todayInUtcMilliseconds()): String =
 	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-		val dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd", Locale.getDefault())
+		val dateFormat = DateTimeFormatter.ofPattern(DATE_PATTERN, Locale.getDefault())
 		Instant.ofEpochMilli(millis)
 			.atZone(ZoneId.systemDefault())
 			.toLocalDate()
 			.format(dateFormat)
 	} else {
-		val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+		val dateFormat = SimpleDateFormat(DATE_PATTERN, Locale.getDefault())
 		dateFormat.format(Date(millis))
 	}
+
+fun parseDate(date: String): Date {
+	try {
+		val dateFormat = SimpleDateFormat(DATE_PATTERN, Locale.getDefault())
+		return dateFormat.parse(date) ?: Date()
+	} catch (e: ParseException) {
+		throw e
+	}
+}
