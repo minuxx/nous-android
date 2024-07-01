2534f391446b80488e8d212b8dc39a83f1c21748
minuxx <minux8905@gmail.com>
2024-07-01 12:47:22 +0900

    :art: chore: 코드 정리
    Resolves: #48

--- app/src/main/java/com/schopenhauer/nous/ui/journal/list/JournalsFragment.kt
+++ app/src/main/java/com/schopenhauer/nous/ui/journal/list/JournalsFragment.kt
@@ -11,33 +11,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.schopenhauer.nous.R
 import com.schopenhauer.nous.databinding.FragmentJournalsBinding
 import com.schopenhauer.nous.ui.base.BaseFragment
-import com.schopenhauer.nous.ui.journal.detail.JournalDetailScreen
 import com.schopenhauer.nous.ui.journal.list.JournalsViewModel.UiEvent
 import com.schopenhauer.nous.ui.main.MainActivity
 import com.schopenhauer.nous.ui.theme.NousTheme
-import com.schopenhauer.nous.util.getTodayTimeMillis
-import com.schopenhauer.nous.util.millisToDate
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
 
 @AndroidEntryPoint
 class JournalsFragment : BaseFragment<FragmentJournalsBinding>() {
 	private val viewModel: JournalsViewModel by viewModels()
-//	private lateinit var journalAdapter: JournalAdapter
 
 	override fun initViews() {
-//		initJournalRecyclerView()
-//		binding.writeJournalFab.setOnClickListener {
-//			(activity as MainActivity).isNotDefaultNavHost = true
-//			findNavController().navigate(R.id.action_journals_to_write_journal)
-//		}
-
 		binding.composeView.apply {
 			setContent {
 				NousTheme {
@@ -63,43 +49,9 @@ class JournalsFragment : BaseFragment<FragmentJournalsBinding>() {
 		}
 	}
 
-
-//	private fun initJournalRecyclerView() {
-//		journalAdapter = JournalAdapter {
-//			(activity as MainActivity).isNotDefaultNavHost = true
-//			val bundle = Bundle().apply { putLong(JOURNAL_ID_KEY, it) }
-//			findNavController().navigate(R.id.action_journals_to_journal_detail, bundle)
-//		}
-//
-//		binding.journalRecyclerView.apply {
-//			layoutManager = LinearLayoutManager(requireActivity())
-//			adapter = journalAdapter
-//			itemAnimator = null
-//			setHasFixedSize(true)
-//		}
-//
-//		binding.journalRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//			override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-//				if (dy < 0 || dy > 0 && binding.writeJournalFab.isShown) binding.writeJournalFab.hide()
-//			}
-//
-//			override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-//				if (newState == RecyclerView.SCROLL_STATE_IDLE) binding.writeJournalFab.show()
-//				super.onScrollStateChanged(recyclerView, newState)
-//			}
-//		})
-//	}
-
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		collectUiState()
 		collectUiEvent()
-	}
-
-	private fun collectUiState() {
-//		collectState(viewModel.uiState.map { it.journals }.distinctUntilChanged()) {
-//			journalAdapter.submitList(it)
-//		}
 	}
 
 	private fun collectUiEvent() {
